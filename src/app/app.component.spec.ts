8c35abe2d5f38ca8dedf952719185e131f8b01d7
Carlos Ramos <carlos.ramos@decoracontent.com>
2019-06-07 11:13:39 -0300

    Added all View Layer tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,9 +13,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatInputModule } from '@angular/material';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ToolbarModule } from './components/toolbar/toolbar.module';
-import {FlexLayoutModule} from '@angular/flex-layout';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { PopupMessageModule } from './components/popup-message/popup-message.module';
 import { ListComponent } from './components/list/list.component';
+
+const displayedColumns = ['robot', 'space', 'rock'];
+let listFixture;
+let list: ListComponent;
+
+function generateMokData() {
+  const data = [];
+  for ( let x = 0; x < 20; x ++) {
+    data.push({name: `Robot_${x}`, space: `Sobot_${x}`, email: `Rock_${x}`});
+  }
+  return data;
+}
 
 
 describe('AppComponent', () => {
@@ -40,35 +52,29 @@ describe('AppComponent', () => {
         AppComponent
       ],
       providers: [HttpClient],
-    }).compileComponents();
+    }).compileComponents().then( () => {
+      listFixture = TestBed.createComponent(ListComponent);
+      list = listFixture.debugElement.componentInstance;
+      list.displayedColumns = displayedColumns;
+      list.list = generateMokData();
+      listFixture.detectChanges();
+    });
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+  it('VIEW layer: should render the App List', () => {
+    expect(list).toBeTruthy();
   });
 
-  it('should render the App List', () => {
-    const displayedColumns = ['robot', 'space', 'rock'];
-    const data = [];
-    for ( let x = 0; x < 20; x ++) {
-      data.push({name: `Robot_${x}`, space: `Sobot_${x}`, email: `Rock_${x}`});
-    }
+  it('VIEW layer: should have columns', () => {
+    const html: string = listFixture.debugElement.nativeElement.innerHTML;
+    const hasAllcolummns = displayedColumns.reduce( (value, column) => value && html.indexOf(column) > -1 , true);
+    expect(hasAllcolummns).toBeTruthy();
+  });
 
-    const fixture = TestBed.createComponent(ListComponent);
-    const list: ListComponent = fixture.debugElement.componentInstance;
-
-    list.displayedColumns = displayedColumns;
-    list.list = data;
-
-    fixture.detectChanges();
-
-    const html: string = fixture.debugElement.nativeElement.innerHTML;
-    const hasAllcolummns = displayedColumns
-      .reduce( (value, column) => value && html.indexOf(column) > -1 , true);
-
-    expect(list && hasAllcolummns).toBeTruthy();
+  it('VIEW layer: should respond to user input', () => {
+    spyOn(list, 'onClick');
+    listFixture.debugElement.nativeElement.querySelectorAll('tr').forEach( e => e.click());
+    listFixture.whenStable().then( () => expect(list.onClick).toHaveBeenCalled());
   });
 
 });
